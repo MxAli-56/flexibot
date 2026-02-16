242f61c59f046eaf741e640d23feb24d6e0615a8
MxAli-56 <muhammadaliasghar905@gmail.com>
2026-02-16 15:12:47 +0500

    created lead.js to capture leads

---
+++ backend/models/Lead.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const LeadSchema = new mongoose.Schema({
+  sessionId: { type: String, required: true },
+  clientId: { type: String, required: true },
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  issue: { type: String, required: true },
+  doctor: { type: String, default: "" }, 
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Lead", LeadSchema);
